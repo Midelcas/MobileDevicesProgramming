f7a1fdc1ddef06a010334f8279dfa06db9736ac9
Midelcas <44088960+Midelcas@users.noreply.github.com>
2018-11-02 20:28:32 +0100

    map fragment

--- app/src/main/java/com/example/midel/stepper/MapFragment.java
+++ app/src/main/java/com/example/midel/stepper/MapFragment.java
@@ -12,18 +12,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback{
     SimpleWalk simpleWalk;
@@ -61,10 +67,31 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
         LatLng start = new LatLng(
                 simpleWalk.getRouteList().get(0).getLongitude(),
                 simpleWalk.getRouteList().get(0).getLatitude());
+        LatLng finish = new LatLng(
+                simpleWalk.getRouteList().get((simpleWalk.getRouteList().size()-1)).getLongitude(),
+                simpleWalk.getRouteList().get((simpleWalk.getRouteList().size()-1)).getLatitude());
         Marker mk = googleMap.addMarker(new MarkerOptions().position(start).title("Start"));
         mk.showInfoWindow(); // Shows the name of the camera in the marker
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(start));
-        googleMap.animateCamera( CameraUpdateFactory.zoomTo( 17.0f ) );
+        Marker mk1 = googleMap.addMarker(new MarkerOptions().position(finish).title("Finish"));
+        mk1.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.finish_run));
+        mk1.showInfoWindow();
+
+        //googleMap.moveCamera(CameraUpdateFactory.newLatLng(start));
+        //googleMap.animateCamera( CameraUpdateFactory.zoomTo( 17.0f ) );
+        List<Marker> markersList = new ArrayList<Marker>();
+        markersList.add(mk);
+        markersList.add(mk1);
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        for (Marker m : markersList) {
+            builder.include(m.getPosition());
+        }
+
+//Bounds padding here
+        int padding = 100;
+
+        //Create bounds here
+        LatLngBounds bounds = builder.build();
+        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
 
         PolylineOptions pLoptions = new PolylineOptions().color(0xFF0000FF);
         for(int i=0; i< simpleWalk.getRouteList().size(); i++){
@@ -72,7 +99,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
                     simpleWalk.getRouteList().get(i).getLatitude()));
         }
         Polyline polyline = googleMap.addPolyline(pLoptions);
-
+        googleMap.animateCamera(cu);
         //radGrp.setOnCheckedChangeListener(new radioGroupCheckedChanged() );
     }
 
